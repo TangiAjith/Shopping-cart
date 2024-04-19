b9b3fefbfc0cd4b8b488df340f1489934192410f
TangiAjith <tangi.ajith@gmail.com>
2024-04-19 11:28:37 +0530

    initial commit

---
+++ src/components/Amazon.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+import React from 'react';
+import list from '../data';
+import '../styles/amazon.css';
+import Cards from './Cards';
+
+const Amazon = ({handleClick}) => {
+  return (
+    <section>
+        {
+            list.map((item)=>(
+                 <Cards item={item} key={item.id} handleClick={handleClick} />
+            ))
+        }
+    </section>
+  )
+}
+
+export default Amazon
